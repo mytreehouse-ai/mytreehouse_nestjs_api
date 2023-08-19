410959bbd06cedd1316d0f225ec83bf534041617
Gelo <chrisgelosulit@gmail.com>
2023-08-19 13:15:47 +0800

    Refactor scraping condition to use ALLOW_SCRAPING config valueThe new condition checks if the ALLOW_SCRAPING config value is set to '1'.

--- src/cheerio/services/cheerio.lamudi.service.ts
+++ src/cheerio/services/cheerio.lamudi.service.ts
@@ -109,7 +109,7 @@ export class CheerioLamudiService {
   async condominiumWithPaging() {
     try {
       // TODO: Remove this soon when fully deployed
-      if (this.configService.get('NODE_ENV') === 'production') {
+      if (this.configService.get('ALLOW_SCRAPING') === '1') {
         return;
       }
 
@@ -216,7 +216,7 @@ export class CheerioLamudiService {
   async houseWithPaging() {
     try {
       // TODO: Remove this soon when fully deployed
-      if (this.configService.get('NODE_ENV') === 'production') {
+      if (this.configService.get('ALLOW_SCRAPING') === '1') {
         return;
       }
 
@@ -321,7 +321,7 @@ export class CheerioLamudiService {
   async landWithPaging() {
     try {
       // TODO: Remove this soon when fully deployed
-      if (this.configService.get('NODE_ENV') === 'production') {
+      if (this.configService.get('ALLOW_SCRAPING') === '1') {
         return;
       }
 
