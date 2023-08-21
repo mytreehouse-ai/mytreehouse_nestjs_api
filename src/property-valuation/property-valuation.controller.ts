06ec4ce44aea832e53b490d33a13ddde6e4b708d
Gelo <chrisgelosulit@gmail.com>
2023-08-21 17:36:04 +0800

    Add condominium property valuation endpoint to PropertyValuationController

--- src/property-valuation/property-valuation.controller.ts
+++ src/property-valuation/property-valuation.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { PropertyValuationService } from './property-valuation.service';
+import { CondominiumPropertyValuationDTO } from 'src/common/dto/condominiumPropertyValuation.dto';
 
 @Controller('property-valuation')
-export class PropertyValuationController {}
+export class PropertyValuationController {
+  constructor(
+    private readonly propertyValuationService: PropertyValuationService,
+  ) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('condominium')
+  condominium(@Body() data: CondominiumPropertyValuationDTO) {
+    return this.propertyValuationService.condominium(data);
+  }
+}
--- src/property-valuation/property-valuation.service.ts
+++ src/property-valuation/property-valuation.service.ts
@@ -1,8 +1,126 @@
 import { Injectable } from '@nestjs/common';
 import { InjectKysely } from 'nestjs-kysely';
+import { sql } from 'kysely';
 import { DB } from 'src/common/@types';
+import { CondominiumPropertyValuationType } from 'src/common/dto/condominiumPropertyValuation.dto';
+import { formatPhp } from 'src/common/utils/formatPhp';
 
 @Injectable()
 export class PropertyValuationService {
   constructor(@InjectKysely() private readonly db: DB) {}
+
+  async condominium(data: CondominiumPropertyValuationType) {
+    const {
+      property_type,
+      listing_type,
+      city,
+      floor_area,
+      year_built,
+      enable_initial_price,
+    } = data;
+
+    const CONDOMINIUM_LIFE_SPAN_IN_YEARS = 50;
+    const INITIAL_PRICE_FOR_SALE = '2_000_000.00';
+    const SOLD_TRANSACTION_ID = '3f49fd58-4060-4cda-bd95-67f612effa9c';
+    const CLOSED_TRANSACTION_ID = 'badda289-30a8-4877-a72b-5cb142bf3b96';
+
+    const valuate = await this.db.transaction().execute(async (trx) => {
+      let closedTransactionAverage = trx
+        .selectFrom('properties')
+        .select(({ fn }) => [
+          fn.avg('properties.current_price').as('average_price'),
+        ])
+        .where('properties.current_price', '>', '0')
+        .where((eb) =>
+          eb.or([
+            eb('properties.property_status_id', '=', SOLD_TRANSACTION_ID),
+            eb('properties.property_status_id', '=', CLOSED_TRANSACTION_ID),
+          ]),
+        )
+        .where('properties.property_type_id', '=', property_type)
+        .where('properties.listing_type_id', '=', listing_type)
+        .where('properties.city_id', '=', city);
+
+      if (enable_initial_price) {
+        closedTransactionAverage = closedTransactionAverage.where(
+          'properties.current_price',
+          '>=',
+          INITIAL_PRICE_FOR_SALE,
+        );
+      } else {
+        closedTransactionAverage = closedTransactionAverage.where(
+          sql`properties.current_price is distinct from 'NaN'::numeric`,
+        );
+      }
+
+      closedTransactionAverage = closedTransactionAverage.where(
+        sql`properties.floor_area between ${floor_area} * 0.8 and ${floor_area} * 1.2`,
+      );
+
+      let scrapedTransactionAverage = trx
+        .selectFrom('properties')
+        .select(({ fn }) => [
+          fn.avg('properties.current_price').as('average_price'),
+        ])
+        .where((eb) =>
+          eb.or([
+            eb('properties.property_status_id', '!=', SOLD_TRANSACTION_ID),
+            eb('properties.property_status_id', '!=', CLOSED_TRANSACTION_ID),
+          ]),
+        )
+        .where('properties.property_type_id', '=', property_type)
+        .where('properties.listing_type_id', '=', listing_type)
+        .where('properties.city_id', '=', city);
+
+      if (enable_initial_price) {
+        scrapedTransactionAverage = scrapedTransactionAverage.where(
+          'properties.current_price',
+          '>=',
+          INITIAL_PRICE_FOR_SALE,
+        );
+      } else {
+        scrapedTransactionAverage = scrapedTransactionAverage.where(
+          sql`properties.current_price is distinct from 'NaN'::numeric`,
+        );
+      }
+
+      scrapedTransactionAverage = scrapedTransactionAverage.where(
+        sql`properties.floor_area between ${floor_area} * 0.8 and ${floor_area} * 1.2`,
+      );
+
+      const closedTransactionAverageResult =
+        await closedTransactionAverage.executeTakeFirstOrThrow();
+
+      const scrapedTransactionAverageResult =
+        await scrapedTransactionAverage.executeTakeFirstOrThrow();
+
+      const closed = parseFloat(
+        closedTransactionAverageResult.average_price?.toString() || '0',
+      );
+
+      const scraped = parseFloat(
+        scrapedTransactionAverageResult.average_price?.toString() || '0',
+      );
+
+      const pricePerSqmInClosedTransaction = closed / floor_area;
+
+      const pricePerSqmInScrapedTransaction = scraped / floor_area;
+
+      const condoRemainingUsefulLife =
+        (CONDOMINIUM_LIFE_SPAN_IN_YEARS -
+          (new Date().getFullYear() - year_built)) /
+        CONDOMINIUM_LIFE_SPAN_IN_YEARS;
+
+      const appraisalValue =
+        (pricePerSqmInClosedTransaction + pricePerSqmInScrapedTransaction) *
+        condoRemainingUsefulLife *
+        floor_area;
+
+      return {
+        appraisalValue: formatPhp(appraisalValue),
+      };
+    });
+
+    return valuate;
+  }
 }
