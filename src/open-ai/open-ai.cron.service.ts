649f1ebbc2f747b867200e1a1e4f500181cedd69
Gelo <chrisgelosulit@gmail.com>
2023-08-27 11:00:10 +0800

    Refactor code to use a constant for unknown city ID

--- src/open-ai/open-ai.cron.service.ts
+++ src/open-ai/open-ai.cron.service.ts
@@ -56,6 +56,8 @@ export class OpenAiCronService {
   @Cron(CronExpression.EVERY_5_SECONDS)
   async updateRecordsWithEmbeddings() {
     try {
+      const UNKNOWN_CITY = '3323750f-270c-43b9-bd46-7c077309c948';
+
       const properties = await this.db
         .selectFrom('properties')
         .innerJoin(
@@ -106,11 +108,7 @@ export class OpenAiCronService {
         .where('properties.ready_to_be_vectorized', '=', true)
         .where('properties.embedding', 'is', null)
         .where('properties.description', 'is not', null)
-        .where(
-          'properties.city_id',
-          '!=',
-          '3323750f-270c-43b9-bd46-7c077309c948',
-        )
+        .where('properties.city_id', '!=', UNKNOWN_CITY)
         .limit(25)
         .execute();
 
